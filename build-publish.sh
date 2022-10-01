573be2f7704361472c64aa01859a71ffdcb4e53f
Michali K <michali@michalikons.com>
2022-10-01 10:01:38 +1000

    Surrounded message variable with double quotes

---
+++ build-publish.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+SC_CHANGES=`git status --porcelain`
+length=${#SC_CHANGES} 
+
+if [ $length -gt 0 ]; then
+echo "There are changes in the working directory. Please commit those changes and rerun the script."
+exit
+fi
+
+bundle exec jekyll build
+echo "Site built"
+
+cp -r _site/. ../michali.github.io/
+MESSAGE=`git log -1 --pretty=%B`
+
+git -C ../michali.github.io add -A
+git -C ../michali.github.io commit -m "$MESSAGE"
+git -C ../michali.github.io push
+
+echo "Pushed to Github pages"
