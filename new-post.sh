a145e23d408173e6de35e5d337562be8fe1e2519
michali <michali@users.noreply.github.com>
2023-10-07 12:03:55 +0000

    deploy: michali/michalikons.com@e8089f3417b320c764ebd3c01f33af6170e5ae75

---
+++ new-post.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <title> [slug]"
+    exit 1
+fi
+
+title="$1"
+
+if [ $# -ge 2 ]; then
+    slug="$2"
+else
+    slug=$(echo "$title" | tr '[:upper:]' '[:lower:]' | tr ' ' '-')
+fi
+
+date=$(date +'%Y-%m-%d')
+
+# Define the file path
+file_path="./_posts/${date}-${slug}.md"
+
+# Create the Markdown file with the specified content
+echo "---" > "$file_path"
+echo "layout: post" >> "$file_path"
+echo "title: \"$title\"" >> "$file_path"
+echo "date: ${date} 08:00+1000" >> "$file_path"
+echo "tags:" >> "$file_path"
+echo "---" >> "$file_path"
+
+echo "Markdown file '$file_path' created successfully."
