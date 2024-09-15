5a356eadcd2bf1c67c2e8eed2035ae2db0e03dc3
michali <michali@users.noreply.github.com>
2024-09-15 01:10:54 +0000

    deploy: michali/michalikons.com@4097e51cd5dcafdf8fdca331cd94302360eab243

--- new-post.sh
+++ new-post.sh
@@ -1,19 +1,29 @@
 #!/bin/sh
 
 if [ $# -lt 1 ]; then
-    echo "Usage: $0 <title> [slug]"
+    echo "Usage: $0 <title> [date] [slug]"
     exit 1
 fi
 
 title="$1"
 
-if [ $# -ge 2 ]; then
-    slug="$2"
+# Check if a date is provided, otherwise use the current date
+if [ $# -ge 2 ] && [ -n "$2" ]; then
+    if ! echo "$2" | grep -Eq '^[0-9]{4}-[0-9]{2}-[0-9]{2}$'; then
+        echo "Invalid date format. Please use YYYY-MM-DD."
+        exit 1
+    fi
+    date="$2"
 else
-    slug=$(echo "$title" | tr '[:upper:]' '[:lower:]' | tr ' ' '-')
+    date=$(date +'%Y-%m-%d')
 fi
 
-date=$(date +'%Y-%m-%d')
+if [ $# -ge 3 ]; then
+    slug="$3"
+else
+    slug=$(echo "$title" | tr '[:upper:]' '[:lower:]' | tr ' ' '-' | tr -d "&’'")
+    slug=$(echo "$slug" | cut -d '-' -f1-7)
+fi
 
 # Define the file path
 file_path="./_posts/${date}-${slug}.md"
